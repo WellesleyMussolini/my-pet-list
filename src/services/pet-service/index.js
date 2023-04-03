a3df6829dc6ef91a8a99a756554e8b3b30975045
wellesleymussolini <iei.muss@gmail.com>
2023-04-03 17:48:22 -0300

    pet service

--- src/services/pet-service/index.js
+++ src/services/pet-service/index.js
@@ -1,5 +1,7 @@
 import { api } from "../api";
 
 export const petService = async (handlePets, route, data) => {
-    return await api.get(route).then((response) => handlePets(response[data])).catch((error) => console.log(error));
+    return await api.get(route).then((response) => handlePets(response[data])).catch(() => {
+        throw new Error("Error while trying to access the pets data");
+    });
 };
