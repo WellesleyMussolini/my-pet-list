dd01704e0a0d614318436f2c9f1a53596f639bfe
wellesleymussolini <iei.muss@gmail.com>
2023-04-20 13:22:54 -0300

    Pets Gallery

--- src/pages/pet-details/components/pet-gallery/PetGallery.js
+++ src/pages/pet-details/components/pet-gallery/PetGallery.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
-const Puppies = styled.div`
-  width: 1000px;
+const Gallery = styled.div`
+  /* width: 1000px;
   background-color: red;
-  height: 400px;
+  height: 600px;
   overflow: hidden;
   display: flex;
   align-items: center;
@@ -12,52 +12,25 @@ const Puppies = styled.div`
   & > img {
     width: 50em;
     object-fit: cover;
-  }
-`;
+  } */
 
-const Slider = styled.input`
+  align-items: center;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  overflow-x: hidden;
   width: 100%;
 `;
 
-const PetGallery = ({ images }) => {
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-    const handleSliderChange = (event) => {
-        setCurrentImageIndex(parseInt(event.target.value));
-    };
-
-    const handleKeyDown = (event) => {
-        if (event.key === "ArrowLeft" && currentImageIndex > 0) {
-            setCurrentImageIndex(currentImageIndex - 1);
-        } else if (event.key === "ArrowRight" && currentImageIndex < images.length - 1) {
-            setCurrentImageIndex(currentImageIndex + 1);
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener("keydown", handleKeyDown);
-        return () => {
-            window.removeEventListener("keydown", handleKeyDown);
-        };
-    });
-
-    const startIndex = Math.max(0, currentImageIndex - 1);
-    const endIndex = Math.min(images.length - 1, currentImageIndex + 1);
+const PetGallery = ({ images, breed }) => {
 
     return (
         <>
-            <Puppies>
-                {images.slice(startIndex, endIndex + 1).map((image, index) => (
-                    <img src={image} alt="puppy" key={startIndex + index} />
+            <Gallery>
+                {images.map((image, index) => (
+                    <img src={image} alt={breed} key={index} />
                 ))}
-            </Puppies>
-            <Slider
-                type="range"
-                min="0"
-                max={images.length - 1}
-                value={currentImageIndex}
-                onChange={handleSliderChange}
-            />
+            </Gallery>
         </>
     );
 };
