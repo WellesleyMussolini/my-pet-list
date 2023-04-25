1d0e948163e0675effb1cff4e81c5fb890e0e429
wellesleymussolini <iei.muss@gmail.com>
2023-04-24 23:44:37 -0300

    FIX - Pet Details

--- src/pages/pet-details/page/Pet.js
+++ src/pages/pet-details/page/Pet.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import LoadingScreen from "../../../components/Loading/Loading";
 import { pet } from "../../../services/pet-service";
-import { Container, BreedVitalStats, Stats, Typography, StatsInfo, Icon, Characteristics, BreedCharacteristics, Puppies, Adult } from "./pet.styles";
-import { petStat } from "../mock/pet.mock";
-import PetsIcon from '@mui/icons-material/Pets';
+import { Container } from "./pet.styles";
 import PetGallery from "../components/pet-gallery/PetGallery";
+import Characteristics from "../components/characteristics/Characteristics";
+import PetStats from "../components/pet-stats/PetStats";
 
 const Pet = () => {
     const { id } = useParams();
@@ -20,83 +20,16 @@ const Pet = () => {
         return <LoadingScreen />
     }
 
-    console.log(dog.breed_characteristics)
-
-    const formatWeight = (weight) => {
-        return `${weight.kg} kg ( ${weight.pounds} lbs )`;
-    }
-
-    const categories = [
-        { name: "FAMILY" },
-        { name: "PHYSICAL" },
-        { name: "SOCIAL" },
-        { name: "HEALTH" }
-    ];
-
-
-
 
     return (
         <Container>
-            <h2>BREED: {dog.breed}</h2>
-            <BreedVitalStats>
-                {petStat.map((pet, index) => {
-                    return (
-                        <Stats key={index}>
-                            <Icon image={pet.icon} />
-                            <Typography>{pet.stat}</Typography>
-                            {pet.information === 'weight' ? (
-                                <StatsInfo>
-                                    <Typography color="gray">Male</Typography>
-                                    <p>
-                                        {formatWeight(dog.weight.male)}
-                                    </p>
-                                    <Typography color="gray">Female</Typography>
-                                    <p>
-                                        {formatWeight(dog.weight.female)}
-                                    </p>
-                                </StatsInfo>
-                            ) : (
-                                <StatsInfo>
-                                    <p>{pet.information.split(".").reduce((object, index) => object[index], dog)}</p>
-                                </StatsInfo>
-                            )}
-                        </Stats>
-                    )
-                })}
-            </BreedVitalStats>
-
-            <BreedCharacteristics>
-                <h1>Breed Characteristics</h1>
-                {categories.map((category, index) => (
-                    <div key={index}>
-                        <h3>{category.name}</h3>
-                        {dog.breed_characteristics[category.name].map((characteristic, index) => (
-                            <Characteristics key={index}>
-                                <h4>
-                                    <strong>{characteristic.characteristics}</strong>
-                                </h4>
-                                <div>
-                                    {[...Array(5)].map((_, index) => (
-                                        <PetsIcon
-                                            key={index}
-                                            color={index < characteristic.points ? "primary" : "disabled"}
-                                        />
-                                    ))}
-                                </div>
-                            </Characteristics>
-                        ))}
-                    </div>
-                ))}
-            </BreedCharacteristics>
+            <h2>{dog.breed}</h2>
+            <PetStats petList={dog} maleWeight={dog.weight.male} femaleWeight={dog.weight.female} />
+            {/* <Characteristics pet={dog.breed_characteristics} /> */}
             <h1>PUPPIES</h1>
-            <div>
-                <PetGallery images={dog.images[1].puppies} />
-            </div>
+            <PetGallery images={dog.images[1].puppies} />
             <h1>ADULT</h1>
-            <div>
-                <PetGallery images={dog.images} />
-            </div>
+            <PetGallery images={dog.images} />
         </Container>
     );
 };
