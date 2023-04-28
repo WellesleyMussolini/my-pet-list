5f836ca6ff53daacaf7ca3ef4e6a13e0c61a4f2c
wellesleymussolini <iei.muss@gmail.com>
2023-04-28 00:11:00 -0300

    feat - header logo

--- src/layout/header/header/Header.js
+++ src/layout/header/header/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { navigate } from "../mock/nav.mock";
 import { Navigate } from "../nav-list/nav.list";
-import { Container, Navigation } from "./header.styles";
+import { Container, Logo, Navigation } from "./header.styles";
+import Bone from "../img/bone.png";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+    const navigator = useNavigate();
     return (
         <Container>
+            <Logo onClick={() => navigator("/")}>
+                <img src={Bone} alt="Bone" />
+            </Logo>
             <Navigation>
                 <Navigate navList={navigate} property="label" />
             </Navigation>
--- src/layout/header/header/header.styles.js
+++ src/layout/header/header/header.styles.js
@@ -1,14 +1,32 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
+    position: relative;
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: center;
     padding: 0 30px;
     height: 80px;
     background: black;
     z-index: 10;
     width: 100%;
+`;
+
+export const Logo = styled.div`
+    position: absolute;
+    left: 25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    & > img{
+        width: 45px;
+        cursor: pointer;
+    }
+
+    & > img:hover{
+        transform: scale(1.1);
+        transition: 0.5s;
+    }
 `;
 
 export const Navigation = styled.ul`
