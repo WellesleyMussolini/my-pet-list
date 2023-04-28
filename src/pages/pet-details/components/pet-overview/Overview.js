4242df6a07fe56078834538827c10fa27ed713ef
wellesleymussolini <iei.muss@gmail.com>
2023-04-27 23:30:56 -0300

    feat - pet overview

---
+++ src/pages/pet-details/components/pet-overview/Overview.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { petStat } from "../../mock/pet.mock";
+import { Container, Icon, Info, Stats, Typography } from "./overview.styles";
+
+const Overview = ({ petList, maleWeight, femaleWeight }) => {
+    const formatWeight = (weight) => {
+        return `${weight.kg} kg`;
+    }
+    const combineWeight = (femaleWeight, maleWeight) => {
+        const minFemale = femaleWeight.kg.substring(0, 2);
+        const maxMale = maleWeight.kg.substring(2);
+        const minFemaleLbs = femaleWeight.pounds.substring(0, 2);
+        const maxMaleLbs = maleWeight.pounds.substring(2);
+
+        const combined = {
+            kg: `${minFemale}${maxMale}`,
+            pounds: `${minFemaleLbs}${maxMaleLbs}`,
+        };
+
+        return formatWeight(combined);
+    };
+
+    const combinedWeight = combineWeight(femaleWeight, maleWeight);
+    console.log(combinedWeight)
+    return (
+        <Container>
+            {
+                petStat.map((pet, index) => {
+                    return (
+                        <Stats key={index}>
+                            <Icon image={pet.icon} />
+                            <Typography color="#E21D23">{pet.stat}</Typography>
+                            {pet.information === "weight" ? (
+                                <Info>{combinedWeight}</Info>
+                            ) : (
+                                <Info>{pet.information.split(".").reduce((object, index) => object[index], petList)}</Info>
+                            )}
+                        </Stats>
+                    )
+                })
+            }
+        </Container>
+    )
+};
+
+export default Overview;
