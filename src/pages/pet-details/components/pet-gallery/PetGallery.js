f0bf84b29de22a5ff16ad7be3a45e51cd118e9a5
wellesleymussolini <iei.muss@gmail.com>
2023-04-27 23:32:20 -0300

    feat - new pet carousel

--- src/pages/pet-details/components/pet-gallery/PetGallery.js
+++ src/pages/pet-details/components/pet-gallery/PetGallery.js
@@ -1,19 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { Container, Controlls, Gallery, Images } from "./pet.gallery.styles";
 
 const PetGallery = ({ images }) => {
+    const petSlider = useRef(null);
+    console.log(petSlider);
+
+    const prevPicture = (event) => {
+        event.preventDefault(); 
+        petSlider.current.scrollLeft -= petSlider.current.offsetWidth;
+
+    };
+
+    const nextPicture = (event) => {
+        event.preventDefault(); 
+        petSlider.current.scrollLeft += petSlider.current.offsetWidth;
+    };
     return (
         <Container>
-            <Gallery>
+            <Gallery ref={petSlider}>
                 {images.map((image, index) => (
                     <Images src={image} alt={`Pet ${index}`} />
                 ))}
             </Gallery>
             <Controlls>
-                <ArrowBackIosNewIcon />
-                <ArrowForwardIosIcon />
+                <ArrowBackIosNewIcon onClick={prevPicture} />
+                <ArrowForwardIosIcon onClick={nextPicture} />
             </Controlls>
         </Container>
     );
