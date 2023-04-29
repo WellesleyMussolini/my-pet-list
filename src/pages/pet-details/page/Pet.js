d1b667783f8926509fce56d44f0abdcbd6016959
wellesleymussolini <iei.muss@gmail.com>
2023-04-29 13:57:16 -0300

    feat - new description

--- src/pages/pet-details/page/Pet.js
+++ src/pages/pet-details/page/Pet.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import LoadingScreen from "../../../components/loading/Loading";
 import { pet } from "../../../services/pet-service";
-import { Container, Title } from "./pet.styles";
+import { Container, Title, Description } from "./pet.styles";
 import PetGallery from "../components/pet-gallery/PetGallery";
 import Characteristics from "../components/characteristics/Characteristics";
 import Overview from "../components/pet-overview/Overview";
@@ -23,12 +23,16 @@ const Pet = () => {
     return (
         <Container>
             <Title>{dog.breed}</Title>
-            <p>{dog.description}</p>
+            <Description>
+                {dog.description.split("\n\n").map((paragraph, index) => (
+                    <p key={index}>{paragraph}</p>
+                ))}
+            </Description>
             <Overview petList={dog} maleWeight={dog.weight.male} femaleWeight={dog.weight.female} />
             <Characteristics pet={dog.breed_characteristics} />
             <h1>PUPPIES</h1>
             <PetGallery images={dog.images.puppies} />
-            <h1>ADULT</h1>
+            <h1>GROWN</h1>
             <PetGallery images={dog.images.gallery} />
         </Container>
     );
--- src/pages/pet-details/page/pet.styles.js
+++ src/pages/pet-details/page/pet.styles.js
@@ -6,13 +6,11 @@ export const Container = styled.div`
     align-items: center;
     flex-direction: column;
     min-height: 100vh;
-    background: #f4f4f4;
+    /* background: #f4f4f4; */
+    background: #EFEFEF;
     overflow: hidden;
     padding: 100px 0;
-    gap: 20px;
-    & > p{
-        width: 55em;
-    };
+    gap: 25px;
 `;
 
 export const Title = styled.h1`
@@ -20,6 +18,13 @@ export const Title = styled.h1`
     font-weight: bolder;
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     color: gray;
+`;
+
+export const Description = styled.div`
+    width: 55em;
+    & > p {
+        margin-bottom: 35px;
+    }
 `;
 
 export const Puppies = styled.div`
