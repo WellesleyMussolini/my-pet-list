a862e4ce07bfade6d3771f882546ef143ee84c6c
wellesleymussolini <iei.muss@gmail.com>
2023-05-01 23:43:26 -0300

    pet loader deleted

--- src/layout/pets/pet-loader/pet.loader.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Wrapper = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 60px;
-    margin: 100px 0;
-    min-height: 700px;
-`;
