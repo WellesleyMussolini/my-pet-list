32b3227a643db097d83f9c972138b3bc23ac7e02
wellesleymussolini <iei.muss@gmail.com>
2023-05-03 14:24:14 -0300

    feat - new error styling

--- src/components/error/Error.js
+++ src/components/error/Error.js
@@ -1,7 +1,7 @@
 import { Text } from "./error.styles";
 
-const Error = ({error}) => {
-    return <Text>{error}</Text>
+const Error = ({error, position, top, left, transform}) => {
+    return <Text style={{ position: position, top: top, left: left, transform: transform }}>{error}</Text>
 };
 
 export default Error;
