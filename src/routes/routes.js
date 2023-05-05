681cf81230f870988c8eab51afb8e27fe2db5ee1
wellesleymussolini <iei.muss@gmail.com>
2023-05-04 23:55:36 -0300

    feat - routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from '../pages/home/page/Home';
+import Home from '../pages/home/Home';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import * as Router from "../constants/routes";
 import Pet from '../pages/pet-details/page/Pet';
