d5df2b4d551d8756abdf3b1c6a80d583b1aba348
wellesleymussolini <iei.muss@gmail.com>
2023-05-05 01:40:48 -0300

    feat - new animation

--- src/layout/pets/pet-list/pet.js
+++ src/layout/pets/pet-list/pet.js
@@ -2,11 +2,17 @@ import React from "react";
 import Card from "../../../components/card/Card";
 import { usePetNavigate } from "../pet-navigate/pet.navigate";
 import { Container } from "./pet.styled";
+import { useSpring } from "react-spring";
 
 const Pets = ({ pets }) => {
     const handleGetPetId = usePetNavigate();
+
+    const props = useSpring({
+        from: { opacity: 0, transform: "translateY(20px)" },
+        to: { opacity: 1, transform: "translateY(0px)" },
+    });
     return (
-        <Container>
+        <Container style={props}>
             {
                 pets.map((pet, index) => {
                     return (
@@ -22,5 +28,6 @@ const Pets = ({ pets }) => {
         </Container>
     );
 };
+
 
 export default Pets;
--- src/layout/pets/pet-list/pet.styled.js
+++ src/layout/pets/pet-list/pet.styled.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import { animated } from "react-spring";
 
-export const Container = styled.div`
+export const Container = styled(animated.div)`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     gap: 60px;
