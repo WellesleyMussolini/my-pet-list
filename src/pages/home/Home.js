6ec84c2aaa8360cdfa7b29a0ef675d1969716888
wellesleymussolini <iei.muss@gmail.com>
2023-05-05 00:52:00 -0300

    feat - new home

---
+++ src/pages/home/Home.js
@@ -0,0 +1,38 @@
+import React, { useState, useMemo } from "react";
+import { useQuery } from "react-query";
+import { pet } from "../../services/pet-service";
+import Input from "../../components/input-search/Input";
+import { Container } from "./home.styles";
+import { pet_filter } from "../../utils/pet.filter";
+import LoadingScreen from "./loader/loader";
+import Error from "../../components/error/Error";
+import Pets from "../../layout/pets/pet-list/pet";
+
+const Home = () => {
+    const [search, setSearch] = useState("");
+
+    const { isLoading, data: pets, error } = useQuery("pets", pet.get);
+
+    const petsFilter = useMemo(() => {
+        return pet_filter(pets, search, "breed");
+    }, [pets, search]);
+
+    if (!isLoading && (!pets || !pets.length)) return <Error message="NO PETS FOUND" centered={true} />
+
+    if (isLoading) return <LoadingScreen />
+
+    if (error) return <Error message="OPS... FAILED TO CONNECT TO API!" centered={true} />
+    return (
+        <Container>
+            <Input handleSearch={(event) => setSearch(event.target.value)} />
+            {
+                petsFilter.length > 0 ?
+                    <Pets pets={petsFilter} />
+                    :
+                    <Error message="NO PET FOUND" centered={false} />
+            }
+        </Container>
+    );
+};
+
+export default Home;
---
+++ src/pages/home/home.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center; 
+    flex-direction: column;
+    min-height: 100vh;
+    gap: 20px;
+    font-size: 1rem;
+    background: #edf0f2;
+`;
