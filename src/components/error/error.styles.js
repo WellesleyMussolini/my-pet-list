4dbf169677a3d9b2b9b5b2e63e7e4b8d4dc2deda
wellesleymussolini <iei.muss@gmail.com>
2023-05-05 00:04:27 -0300

    fix - styles

--- src/components/error/error.styles.js
+++ src/components/error/error.styles.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
 export const Text = styled.h1`
+    display: flex;
+    align-items: center;
     text-align: center;
     font-weight: bolder;
     font-size: 2.2em;
     color: rgb(204, 68, 68);
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    height: 700px;
+    margin: 0;
 `;
