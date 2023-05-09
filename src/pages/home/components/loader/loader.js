e599874c4ab5c9bd2295e2defef82030184fb29e
wellesleymussolini <iei.muss@gmail.com>
2023-05-09 00:06:07 -0300

    Feature - Home Loader

---
+++ src/pages/home/components/loader/loader.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Wrapper, LoadingCards } from "./loader.styles";
+import Loading from "../../../../components/loading/Loading";
+import { cardSkeleton } from "../../utils/card-skeleton";
+import CardSkeleton from "../../../../components/card-skeleton/CardSkeleton";
+
+const card_amount = 5;
+
+const LoadingScreen = () => {
+    const petSkeleton = cardSkeleton(CardSkeleton, card_amount);
+    return (
+        <Container>
+            <div></div>
+            <Wrapper>
+                <LoadingCards>
+                    <Loading />
+                    {petSkeleton}
+                </LoadingCards>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default LoadingScreen;
---
+++ src/pages/home/components/loader/loader.styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-left: auto;
+    margin-right: auto;
+    min-height: 100vh;
+    gap: 20px;
+    background: #edf0f2;
+
+    & > div:first-child{
+        height: 100px;
+        margin-top: 80px;
+    }
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+export const LoadingCards = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 60px;
+    height: 700px;
+`;
