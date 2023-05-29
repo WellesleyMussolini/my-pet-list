159e26a4e8413b6c7521689ffae82618435a387e
wellesleymussolini <iei.muss@gmail.com>
2023-05-29 20:12:44 -0300

    Feature - Header icons styled

--- src/layout/header/mock/nav.mock.js
+++ src/layout/header/mock/nav.mock.js
@@ -1,3 +1,7 @@
+import { Brazil } from "../img/brazil.svg";
 import Bone from "../img/bone.png";
 
-export const navigate = [{ label: <img src={Bone} alt="Bone" />, route: "/" }];
+export const navigate = [
+    { label: <img src={Bone} alt="Bone" />, route: "/" },
+    // { label: <Brazil />, route: "#" },
+];
