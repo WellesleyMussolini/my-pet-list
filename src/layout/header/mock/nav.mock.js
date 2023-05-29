ba04a3763eed3c8c8b4d453674abeed272ce829b
wellesleymussolini <iei.muss@gmail.com>
2023-05-29 20:15:15 -0300

    Feature - header elements

--- src/layout/header/mock/nav.mock.js
+++ src/layout/header/mock/nav.mock.js
@@ -2,6 +2,8 @@ import { Brazil } from "../img/brazil.svg";
 import Bone from "../img/bone.png";
 
 export const navigate = [
-    { label: <img src={Bone} alt="Bone" />, route: "/" },
+    {
+        label: <img src={Bone} alt="Bone" />, route: "/"
+    },
     // { label: <Brazil />, route: "#" },
 ];
