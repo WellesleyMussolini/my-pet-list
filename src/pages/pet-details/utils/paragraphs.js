1e36eddb275dde32a7c56df1e5d876642b42268c
wellesleymussolini <iei.muss@gmail.com>
2023-06-01 20:19:47 -0300

    description paragraphs

--- src/pages/pet-details/utils/paragraphs.js
+++ src/pages/pet-details/utils/paragraphs.js
@@ -1,3 +1,1 @@
-export const paragraphs = (description) => {
-    return description.split("\n\n").map((paragraph, index) => <p key={index}>{paragraph}</p>);
-}
+export const paragraphs = description => description.split("\n\n").map((paragraph, index) => <p key={index}>{paragraph}</p>);
