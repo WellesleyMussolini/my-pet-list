a8cc0738f6662dfc65a0004a852bffceb1b00f2a
wellesleymussolini <iei.muss@gmail.com>
2023-06-01 20:18:58 -0300

    language options available

---
+++ src/layout/header/language/utils/languageOptions.js
@@ -0,0 +1,7 @@
+import { Brazil } from "../../svg/brazil.svg";
+import { USA } from "../../svg/usa.svg";
+
+export const languageOptions = [
+    { label: <USA />, value: "usa" },
+    { label: <Brazil />, value: "br" }
+];
