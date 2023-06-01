22cccd7f5948d5d1ac3146cbc7ac12a07f63898d
wellesleymussolini <iei.muss@gmail.com>
2023-06-01 20:18:31 -0300

    language option choice

--- src/layout/header/language/Languages.js
+++ src/layout/header/language/Languages.js
@@ -1,16 +1,10 @@
 import React from "react";
-import { Brazil } from "../svg/brazil.svg";
-import { USA } from "../svg/usa.svg";
 import { SelectStyles } from "./languages.styles";
-
-const languages = [
-    { label: <USA />, value: "usa" },
-    { label: <Brazil />, value: "br" }
-];
+import { languageOptions } from "./utils/languageOptions";
 
 export const Languages = () => {
-    const [selectedLanguage, setSelectedLanguage] = React.useState(languages[0]);
+    const [selectedLanguage, setSelectedLanguage] = React.useState(languageOptions[0]);
     const handleLanguage = (language) => setSelectedLanguage(language);
 
-    return <SelectStyles options={languages} onChange={handleLanguage} value={selectedLanguage} />;
+    return <SelectStyles options={languageOptions} onChange={handleLanguage} value={selectedLanguage} />;
 };
