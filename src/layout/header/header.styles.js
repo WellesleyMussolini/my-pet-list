f95f1c88aa4d2d3125741d22eac729f99d6af931
wellesleymussolini <iei.muss@gmail.com>
2023-06-01 20:18:13 -0300

    styles

--- src/layout/header/header.styles.js
+++ src/layout/header/header.styles.js
@@ -7,7 +7,7 @@ export const Container = styled.header`
     padding: 0 30px;
     height: 80px;
     background: rgb(8, 97, 170);
-    z-index: 10;
+    z-index: 30;
     width: 100%;
     transition: all 0.3s;
     opacity: ${(props) => (props.scrollDown ? "0.6" : "1")};
