ab48fdf904605dc71d8cc42d1802d72a108d1da2
wellesleymussolini <iei.muss@gmail.com>
2023-06-08 00:57:59 -0300

    Implement SVG icons for language selection

--- src/layout/header/language/utils/languageOptions.js
+++ src/layout/header/language/utils/languageOptions.js
@@ -1,7 +1,7 @@
 import { Brazil } from "../../svg/brazil.svg";
 import { USA } from "../../svg/usa.svg";
 
-export const languageOptions = [
-    { label: <USA />, value: "us" },
-    { label: <Brazil />, value: "pt-br" }
-];
+export const languageOptions = {
+    english: { label: <USA />, value: "en" },
+    portuguese: { label: <Brazil />, value: "ptBr" }
+};
