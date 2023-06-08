302bcc0e71243ca64adb798681b6e8f509cdea78
wellesleymussolini <iei.muss@gmail.com>
2023-06-08 00:56:34 -0300

    Integrate language selection feature

--- src/layout/header/language/Languages.js
+++ src/layout/header/language/Languages.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { SelectStyles } from "./languages.styles";
 import { languageOptions } from "./utils/languageOptions";
+import { UseLanguage } from "../../../context/languageContext";
 
 export const Languages = () => {
-    const [selectedLanguage, setSelectedLanguage] = React.useState(languageOptions[0]);
-    const handleLanguage = (language) => setSelectedLanguage(language);
+    const { language, setLanguage } = UseLanguage();
+    const handleLanguage = (language) => setLanguage(language);
 
-    return <SelectStyles options={languageOptions} onChange={handleLanguage} value={selectedLanguage} />;
+    const options = Object.keys(languageOptions).map((key) => {
+        const language = languageOptions[key];
+        return {
+            label: language.label,
+            value: language.value,
+        };
+    });
+
+    return <SelectStyles options={options} onChange={handleLanguage} value={language} />;
 };
