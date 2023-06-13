8052788bb6382c54aaadef47afdc188bc085e270
wellesleymussolini <iei.muss@gmail.com>
2023-06-13 13:45:16 -0300

    Language selection functionality with context

--- src/context/languageContext.js
+++ src/context/languageContext.js
@@ -6,10 +6,10 @@ const Context = React.createContext();
 export const LanguageContextProvider = ({ children }) => {
     const [language, setLanguage] = React.useState(languageOptions.english);
     return <Context.Provider value={{ language, setLanguage }}>{children}</Context.Provider>
-}
+};
 
 export const UseLanguage = () => {
     const context = React.useContext(Context);
     const { language, setLanguage } = context;
     return { language, setLanguage };
-}
+};
