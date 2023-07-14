dba4a226c47288f9006816291b7c31741b3d1fdd
WellesleyMussolini <iei.muss@gmail.com>
2023-07-14 13:42:04 -0300

    Implement loader feature

--- src/pages/home/components/loader/loader.component.js
+++ src/pages/home/components/loader/loader.component.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { 
-    Container, 
-    Wrapper, LoadingCards, Silhouette } from "./loader.styles";
+import { Container, Wrapper, LoadingCards, Silhouette } from "./loader.styles";
 import Loading from "../../../../components/loading/loading.component";
 import { cardSkeleton } from "../../utils/card-skeleton";
 import CardSkeleton from "../../../../components/card-skeleton/CardSkeleton";
 
 const card_amount = 5;
 
-const LoadingScreen = () => {
+const Loader = () => {
     const petSkeleton = cardSkeleton(CardSkeleton, card_amount);
     return (
         <Container>
@@ -23,4 +21,4 @@ const LoadingScreen = () => {
     );
 };
 
-export default LoadingScreen;
+export default Loader;
