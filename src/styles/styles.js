d65a976b9a9218b25d1460f9c512b982c34000ce
WellesleyMussolini <iei.muss@gmail.com>
2023-07-17 19:48:21 -0300

    Set up initial global styling setup

---
+++ src/styles/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Styles = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background: #edf0f2;
+`;
