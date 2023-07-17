9d3611b99fc31ac6e749f231dc564e34ad7c023c
WellesleyMussolini <iei.muss@gmail.com>
2023-07-17 19:39:40 -0300

    Import necessary modules and create route structure

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,10 +3,11 @@ import Home from '../pages/home/Home';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import * as Router from "../constants/routes";
 import Pet from '../pages/pet-details/Pet';
-import NotFound from '../components/not-found/NotFound';
+import NotFound from '../components/not-found/notfound.component';
 import Header from '../layout/header/header.component';
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { LanguageContextProvider } from '../context/languageContext';
+import { Styles } from '../styles/styles';
 
 const queryClient = new QueryClient();
 
@@ -15,11 +16,13 @@ const Routers = () => (
         <QueryClientProvider client={queryClient}>
             <LanguageContextProvider>
                 <Header />
-                <Routes>
-                    <Route exact path={Router.HOME} element={<Home />} />
-                    <Route path={Router.PET} element={<Pet />} />
-                    <Route path={Router.NOT_FOUND} element={<NotFound />} />
-                </Routes>
+                <Styles>
+                    <Routes>
+                        <Route exact path={Router.HOME} element={<Home />} />
+                        <Route path={Router.PET} element={<Pet />} />
+                        <Route path={Router.NOT_FOUND} element={<NotFound />} />
+                    </Routes>
+                </Styles>
             </LanguageContextProvider>
         </QueryClientProvider>
     </BrowserRouter>
