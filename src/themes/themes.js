49d226847503723f556443cff26db86b241c9a1b
WellesleyMussolini <iei.muss@gmail.com>
2023-07-19 14:29:41 -0300

    Themes configuration file to centralize project styling settings

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -1,7 +1,28 @@
 export const Themes = {
     primary: {
-        default: "",
-        hover: "",
+        default: "#009DFF",
+        focus: "#1E90FF" || "dodgerblue",
+        hover: "#0083D4"
     },
-    background: "",
-}
+    error: {
+        default: "#D92C2C",
+        focus: "#C51818",
+        hover: "#BB0E0E"
+    },
+    alert: {
+        default: "#FFCC00",
+        hover: "#f3b005",
+    },
+    disabled: {
+        default: "#00000042",
+        hover: "#AAAAAA",
+    },
+    background: "#edf0f2",
+    cancel: "#fefefe",
+    gray: "#9b9b9b",
+    darkgray: "#626262",
+    black: "#000",
+    white: "#fff",
+    blue: "#24A9FB",
+    darkblue: "#0861AA",
+};
